ebe0f8c68fa96c674af992bcbd8d10cda2a8c6d1
Tamar Mekhrishvili <tmekh11@freeuni.edu.ge>
2021-09-28 10:53:44 +0400

    feat: funcs

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -23,8 +23,8 @@
  *   getComposition(Math.sin, Math.asin)(x) => Math.sin(Math.asin(x))
  *
  */
-function getComposition(/* f, g */) {
-  throw new Error('Not implemented');
+function getComposition(f, g) {
+  return (x) => f(g(x));
 }
 
 
@@ -44,8 +44,8 @@ function getComposition(/* f, g */) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return (x) => x ** exponent;
 }
 
 
@@ -62,8 +62,14 @@ function getPowerFunction(/* exponent */) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...params) {
+  if (params.length === 0) return null;
+  const a = params[0];
+  const b = params[1];
+  const c = params[2];
+  if (params.length === 2) return (x) => x + b;
+  if (params.length === 1) return () => a;
+  return (x) => a * x ** a + b * x + c;
 }
 
 
@@ -81,8 +87,12 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let mem;
+  return () => {
+    if (!mem) mem = func();
+    return mem;
+  };
 }
 
 
@@ -169,8 +179,14 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let starter = startFrom;
+  function increase() {
+    const previous = starter;
+    starter += 1;
+    return previous;
+  }
+  return increase;
 }
 
 
